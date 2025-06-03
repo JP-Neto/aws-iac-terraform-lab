0f31c08d91464a49974275aa5793e216b9a3cd93
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2025-06-03 14:56:30 -0300

    Update vm_windowns.tf

--- Aplicacao/vm_windowns.tf
+++ Aplicacao/vm_windowns.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "windowns-ec2-neto" {
   ami                    = var.ami-win
-  availability_zone      = var.azs[1] #Precisa melhorar
+  availability_zone      = var.azs[1] 
   instance_type          = var.instance_type
   key_name               = var.aws-key
-  subnet_id              = var.subnetPrivateIds[0]    #Precisa melhorar Subnet APP
-  vpc_security_group_ids = [var.segGroupPrivateId[0]] #Precisa melhorar Subnet APP
+  subnet_id              = var.subnetPrivateIds[0]    
+  vpc_security_group_ids = [var.segGroupPrivateId[0]]
 
   tags = { Name = "Ec2 Windowns - Neto - Rdp" }
 
